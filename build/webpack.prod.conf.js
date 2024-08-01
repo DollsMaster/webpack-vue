6f9d5987371877d7e5f95aa6e0e2d89067acf3d4
zhanghan <1599252137@qq.com>
2024-08-01 16:59:11 +0800

    webpack增加环境配置

---
+++ build/webpack.prod.conf.js
@@ -0,0 +1,10 @@
+const {merge} = require("webpack-merge");
+const baseConfig = require('./webpack.base.conf')
+const config = require('../config/index')
+const webpackProdConf = merge(baseConfig, {
+  mode: 'prduction',
+  module: {
+    rules: []
+  },
+  devtool: config.build.devtool ? config.build.devtool : false
+})

98862bdfb1931cb8c139fae51acf8b0426704d96
zhanghan <1599252137@qq.com>
2024-09-05 10:14:58 +0800

    打包逻辑初步完成

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,12 +1,30 @@
 const {merge} = require("webpack-merge");
 const baseConfig = require('./webpack.base.conf')
 const config = require('../config/index')
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const webpackProdConf = merge(baseConfig, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: path.posix.join('static', 'js/chunk-[chunkhash:8].js'),
+    chunkFilename: path.posix.join('static', 'js/chunk-[chunkhash:8].js')
+  },
   module: {
     rules: []
   },
-  devtool: config.build.devtool ? config.build.devtool : false
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: path.posix.join('static', 'css/[name].[contenthash:8].js'),
+      chunkFilename: path.posix.join('static', 'css/[name].[contenthash:8].js')
+    })
+  ],
+  devtool: false,
+  optimization: {
+    chunkIds: 'deterministic'
+  }
+
 })
 
 module.exports = webpackProdConf

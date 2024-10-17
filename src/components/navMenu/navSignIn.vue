5ad81357a52e763fa141f8d81d1c3e94a39efa5d
zhanghan <1599252137@qq.com>
2024-10-17 22:13:33 +0800

    系统首页

--- src/components/navMenu/navSignIn.vue
+++ src/components/navMenu/navSignIn.vue
@@ -1,22 +1,76 @@
 <script setup>
-
+import {ref} from 'vue'
+const siginVisable = ref(false)
 </script>
 
 <template>
-  <div class="nav-sigin">
-    <span>签到</span>
+  <div class="nav-sigin-wrap">
+    <div class="nav-sigin">
+      <span>签到</span>
+    </div>
+
   </div>
 </template>
 
 <style scoped lang="scss">
+.nav-sigin-wrap {
+  position: relative;
+
+}
 .nav-sigin {
   padding: 1rem 1.5rem;
   height: 100%;
   color: var(--sys-white-word);
   background-color: var(--sys-grey-isActive);
+  cursor: pointer;
   &:hover {
     transition: all 2s;
-    transform: translateX();
+    animation-name: navSigin;
+    animation-duration: 0.5s;
+    transform: rotate(0);
+    &:before {
+      position: absolute;
+      left: 40%;
+      top: 100%;
+      width: 0;
+      height: 0;
+      border-style: solid;
+      border-width: 0 5px 8.7px 5px;
+      border-color: rgba(0, 0, 0, 0) rgba(0, 0, 0, 0) #525e54 rgba(0, 0, 0, 0);
+      content: "";
+      z-index: 10;
+    }
+    &:after {
+      position: absolute;
+      z-index: 1;
+      top: 100%;
+      left: -1.5rem;
+      margin-top: 7px;
+      padding: 0.3rem 3rem;
+      border: solid 1px var(--sys-grey-isActive);
+      white-space: nowrap;
+      border-radius: 1rem;
+      color: var(--sys-grey-isActive);
+      content: '签到';
+      background-color: #ffffff;
+    }
+  }
+}
+.sigin-btn {
+
+}
+@keyframes navSigin {
+  0% {
+  transform: rotate(0deg)
+  }
+  30% {
+    transform: rotate(20deg)
+  }
+  60% {
+    transform: rotate(-20deg)
+  }
+  100% {
+    transform: rotate(0deg)
   }
 }
 </style>
--- src/components/navMenu/userInfo.vue
+++ src/components/navMenu/userInfo.vue
@@ -1,24 +1,57 @@
 <script setup>
-
+import {ref} from 'vue'
+const dialogVisable = ref(false)
 </script>
 
 <template>
-  <div class="user-info">
-    <img src="../../assets/userAvaer.jpg" alt="">
+  <div class="user-info-wrap" @mouseover="dialogVisable = true" @mouseout="dialogVisable = false">
+    <div class="user-info">
+      <img src="../../assets/userAvaer.jpg" alt="">
+    </div>
+    <div class="user-info-dialog" :class="[dialogVisable ? 'is-active' : '']" v-show="dialogVisable">
+
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .user-info {
-    display: flex;
-    margin: .5rem;
-    img {
-      width: 100%;
-      height: 100%;
-      border-radius: 100%;
-    }
-    width: 2.3rem;
-    height: 2.3rem;
-
+.user-info-wrap {
+  position: relative;
+}
+.user-info {
+  position: relative;
+  z-index: 10;
+  display: flex;
+  margin: .5rem;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
   }
+  width: 2.3rem;
+  height: 2.3rem;
+}
+.is-active {
+  animation-name: isActiveAnimation;
+  animation-duration: 1s;
+  transform: scale(1, 1);
+}
+.user-info-dialog {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 15rem;
+  height: 9rem;
+  border: solid 1px red;
+  background-color: white;
+}
+
+@keyframes isActiveAnimation {
+  0% {
+    transform: scale(0.1, 0.1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
+}
 </style>
